80e38592e12a494806870ebc91c0ba9c6d60e445
Facundo Nostas <155680624+FacundoN100@users.noreply.github.com>
2025-07-01 14:40:55 -0300

    Update PlayerController.cs

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+
+using System.Collections;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Character))]
@@ -6,17 +8,29 @@ public class PlayerController : MonoBehaviour, ISetup<IPlayerControllerModel>
 {
     public IPlayerControllerModel Model { get; set; }
 
-    private bool _isJumping;
-    private bool _isDoubleJumping;
-    private Character _character;
-    private Coroutine _jumpCoroutine;
+    private ICharacter _character;
+    private StateMachine _stateMachine;
+    private IJumpAbility _jumpAbility;
 
     private void Awake()
-        => _character = GetComponent<Character>();
+    {
+        _character = (ICharacter)GetComponent<Character>();
+    }
 
     public void Setup(IPlayerControllerModel model)
     {
         Model = model;
+
+        
+        _stateMachine = new StateMachine();
+        _stateMachine.Initialize(
+            new GroundState(_stateMachine, Model.AirborneSpeedMultiplier),
+            _character
+        );
+
+        
+        _jumpAbility = new DoubleJumpAbility(_stateMachine, _character);
+
         if (Model.MoveInput?.action != null)
         {
             Model.MoveInput.action.started += HandleMoveInput;
@@ -26,10 +40,12 @@ public class PlayerController : MonoBehaviour, ISetup<IPlayerControllerModel>
         if (Model.JumpInput?.action != null)
             Model.JumpInput.action.performed += HandleJumpInput;
     }
+
     private void OnDisable()
     {
         if (Model.MoveInput?.action != null)
         {
+            Model.MoveInput.action.started -= HandleMoveInput;
             Model.MoveInput.action.performed -= HandleMoveInput;
             Model.MoveInput.action.canceled -= HandleMoveInput;
         }
@@ -39,42 +55,154 @@ public class PlayerController : MonoBehaviour, ISetup<IPlayerControllerModel>
 
     private void HandleMoveInput(InputAction.CallbackContext ctx)
     {
-        var direction = ctx.ReadValue<Vector2>().x;
-        if (_isJumping || _isDoubleJumping)
-            direction *= Model.AirborneSpeedMultiplier;
-        _character?.SetDirection(direction);
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        _stateMachine.Move(dir);
     }
 
     private void HandleJumpInput(InputAction.CallbackContext ctx)
     {
-        if (_isJumping)
-        {
-            if (_isDoubleJumping)
-                return;
-            RunJumpCoroutine();
-            _isDoubleJumping = true;
-            return;
-        }
-        RunJumpCoroutine();
-        _isJumping = true;
-    }
-
-    private void RunJumpCoroutine()
-    {
-        if (_jumpCoroutine != null)
-            StopCoroutine(_jumpCoroutine);
-        _jumpCoroutine = StartCoroutine(_character.Jump());
+        _jumpAbility.TryJump();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         foreach (var contact in other.contacts)
         {
-            if (Vector3.Angle(contact.normal, Vector3.up) < 5)
+            if (Vector3.Angle(contact.normal, Vector3.up) < 5f)
             {
-                _isJumping = false;
-                _isDoubleJumping = false;
+                _jumpAbility.OnLand();
+                _stateMachine.Land();
             }
         }
     }
+}
+
+public interface IJumpAbility
+{
+   
+    bool TryJump();
+    
+    void OnLand();
+}
+
+public class DoubleJumpAbility : IJumpAbility
+{
+    private readonly StateMachine _sm;
+    private readonly ICharacter _character;
+    private int _jumpsDone;
+
+    public DoubleJumpAbility(StateMachine sm, ICharacter character)
+    {
+        _sm = sm;
+        _character = character;
+        _jumpsDone = 0;
+    }
+
+    public bool TryJump()
+    {
+        if (_jumpsDone >= 2)
+            return false;
+
+        if (_jumpsDone == 0)
+        {
+            
+            _sm.Jump();
+        }
+        else
+        {
+            
+            if (_character is MonoBehaviour mb)
+                mb.StartCoroutine(_character.Jump());
+        }
+
+        _jumpsDone++;
+        return true;
+    }
+
+    public void OnLand()
+    {
+        _jumpsDone = 0;
+    }
+}
+
+
+public interface IPlayerState
+{
+    void Enter(ICharacter character);
+    void HandleMove(ICharacter character, Vector2 direction);
+    void HandleJump(ICharacter character);
+    void HandleLand(ICharacter character);
+    void Exit(ICharacter character);
+}
+
+public class StateMachine
+{
+    private IPlayerState _current;
+    private ICharacter _character;
+
+    public void Initialize(IPlayerState startState, ICharacter character)
+    {
+        _character = character;
+        _current = startState;
+        _current.Enter(_character);
+    }
+
+    public void ChangeState(IPlayerState nextState)
+    {
+        _current.Exit(_character);
+        _current = nextState;
+        _current.Enter(_character);
+    }
+
+    public void Move(Vector2 direction) => _current.HandleMove(_character, direction);
+    public void Jump() => _current.HandleJump(_character);
+    public void Land() => _current.HandleLand(_character);
+}
+
+public class GroundState : IPlayerState
+{
+    private readonly StateMachine _sm;
+    private readonly float _airborneSpeed;
+
+    public GroundState(StateMachine sm, float airborneSpeed)
+    {
+        _sm = sm;
+        _airborneSpeed = airborneSpeed;
+    }
+
+    public void Enter(ICharacter c) { }
+    public void HandleMove(ICharacter c, Vector2 dir) => c.SetDirection(dir.x);
+
+    public void HandleJump(ICharacter c)
+    {
+        if (c is MonoBehaviour mb)
+            mb.StartCoroutine(c.Jump());
+        _sm.ChangeState(new JumpState(_sm, _airborneSpeed));
+    }
+
+    public void HandleLand(ICharacter c) { }
+    public void Exit(ICharacter c) { }
+}
+
+public class JumpState : IPlayerState
+{
+    private readonly StateMachine _sm;
+    private readonly float _airborneSpeed;
+
+    public JumpState(StateMachine sm, float airborneSpeed)
+    {
+        _sm = sm;
+        _airborneSpeed = airborneSpeed;
+    }
+
+    public void Enter(ICharacter c) { }
+    public void HandleMove(ICharacter c, Vector2 dir) => c.SetDirection(dir.x * _airborneSpeed);
+    public void HandleJump(ICharacter c) { }
+
+    public void HandleLand(ICharacter c)
+    {
+        _sm.ChangeState(new GroundState(_sm, _airborneSpeed));
+    }
+
+    public void Exit(ICharacter c) { }
 }
