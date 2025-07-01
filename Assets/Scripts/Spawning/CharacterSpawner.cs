3d81bfafd99d1ac707b05368cfbc740cf46610ca
Facundo Nostas <155680624+FacundoN100@users.noreply.github.com>
2025-07-01 14:44:25 -0300

    Update CharacterSpawner.cs

--- Assets/Scripts/Spawning/CharacterSpawner.cs
+++ Assets/Scripts/Spawning/CharacterSpawner.cs
@@ -1,26 +1,61 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Transform))]
 public class CharacterSpawner : MonoBehaviour
 {
+    
+    public static CharacterSpawner Instance { get; private set; }
+
+    [Header("Prefab del Character")]
     [SerializeField] private Character prefab;
+
+    [Header("Model de Character y Controller")]
     [SerializeField] private CharacterModel characterModel;
     [SerializeField] private PlayerControllerModel controllerModel;
+
+    [Header("Animator Override (Opcional)")]
     [SerializeField] private RuntimeAnimatorController animatorController;
 
+    private void Awake()
+    {
+       
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        
+        GameServiceRegistry.Add<CharacterSpawner>(this);
+    }
+
+   
     public void Spawn()
     {
-        var result = Instantiate(prefab, transform.position, transform.rotation);
-        if (!result.TryGetComponent(out Character character))
-            character = result.gameObject.AddComponent<Character>();
-        character.Setup(characterModel);
+        if (prefab == null)
+        {
+            Debug.LogError(" Prefab (Character) no asignado en CharacterSpawner.");
+            return;
+        }
 
-        if (!result.TryGetComponent(out PlayerController controller))
+      
+        var result = Instantiate(prefab, transform.position, transform.rotation);
+
+     
+        result.Setup(characterModel);
+
+       
+        if (!result.TryGetComponent<PlayerController>(out var controller))
             controller = result.gameObject.AddComponent<PlayerController>();
         controller.Setup(controllerModel);
 
-        var animator = result.GetComponentInChildren<Animator>();
-        if (!animator)
-            animator = result.gameObject.AddComponent<Animator>();
-        animator.runtimeAnimatorController = animatorController;
+       
+        if (animatorController != null)
+        {
+            var anim = result.GetComponentInChildren<Animator>();
+            if (anim != null)
+                anim.runtimeAnimatorController = animatorController;
+        }
     }
 }
