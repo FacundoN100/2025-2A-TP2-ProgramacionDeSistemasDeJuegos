af50bd8103ce0142d4aac38a21ffa5d0e5eea11a
Facundo Nostas <155680624+FacundoN100@users.noreply.github.com>
2025-07-01 14:34:03 -0300

    Update Character.cs

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using UnityEngine;
 
+
 [RequireComponent(typeof(Rigidbody2D))]
-public class Character : MonoBehaviour, ISetup<CharacterModel>
+public class Character : MonoBehaviour, ISetup<CharacterModel>, ICharacter
 {
     private float _direction = 0;
     private Rigidbody2D _rigidbody;
     [field: SerializeField] public CharacterModel Model { get; set; } = new();
 
     public Vector2 Velocity => _rigidbody?.linearVelocity ?? Vector2.zero;
+
     private void Awake()
         => _rigidbody = GetComponent<Rigidbody2D>();
 
-    
     public void Setup(CharacterModel model)
         => Model = model;
 
@@ -23,12 +24,16 @@ public class Character : MonoBehaviour, ISetup<CharacterModel>
             _rigidbody.AddForce(scaledDirection, ForceMode2D.Force);
     }
 
+ 
     public void SetDirection(float direction)
         => _direction = direction;
 
+   
     public IEnumerator Jump()
     {
         yield return new WaitForFixedUpdate();
         _rigidbody.AddForce(Vector3.up * Model.JumpForce, ForceMode2D.Impulse);
     }
+
+  
 }
